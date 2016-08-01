451ef3b940fa45a1c092563321165ddffeb4d8fe
Fengda HUANG <h@phodal.com>
2016-08-02 07:45:51 +0800

    update titles & remove some unused codes

--- assets/js/opensuse-theme.js
+++ assets/js/opensuse-theme.js
@@ -251,46 +251,6 @@ function backToMainPageOs (os) {
 
 }
 
-//check if there is a langCookie in the browser
-$(document).on("ready", function(){
-
-  var languageCode;
-  var selectedLanguageName;
-
-  if (cookieLanguage === undefined) {
-    try {
-      // try to use navigator.language
-      languageCode = navigator.language.replace("-","_");
-      window.lang.change(languageCode);
-    }
-    catch(err) {
-      // navigator.language is not available
-      if (navigator.language.length > 2) {
-        try {
-          // try with a more general string (for example, if navigator.language is "es-ES" then "es" is tried)
-          languageCode = navigator.language.substring(0,2);
-          window.lang.change(languageCode);
-        }
-        catch(err) {
-          languageCode = "en";
-        }
-      }
-      else {
-        languageCode = "en";
-      }
-    }
-  }
-  else {
-    languageCode = cookieLanguage;
-  }
-
-  selectedLanguageName = $(".languages").find("[data-language-value='" + languageCode + "']").html();
-  $(".selected-language").html(selectedLanguageName);
-
-});
-//*****************
-
-
 //init particles background
 /* Disabled for excessive CPU consumption
 $(document).ready(function() {
@@ -382,11 +342,6 @@ var contributionInterpolation = function () {
 
 }
 
-// Add "openSUSE Linux OS" to the document Title
-$(document).on("ready", function() {
-  $("title").prepend('openSUSE - Linux OS. ')
-})
-
 // check if SVG is supported by the user's browser
 $(document).on("ready", function(){
   if (!Modernizr.svg) {
@@ -400,25 +355,6 @@ $(document).on("ready", function(){
     $("#contribute-to-opensuse").addClass('no-svg')
   }
 })
-
-// Facebook token
-window.fbAsyncInit = function() {
-  FB.init({
-    appId : '831347386978737',
-    xfbml : true,
-    version : 'v2.4'
-  });
-};
-
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
 
 window.onkeyup = function(e) {
   var key = e.keyCode ? e.keyCode : e.which;
